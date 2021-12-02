eef3d6cd61a162dc03bca08133d532bd8431f11e
Vlad Nicola <vlad.nicola@axigen.com>
2021-12-02 19:43:34 +0200

    Day02 solution

---
+++ Day02/Day02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include <numeric>
+
+int main()
+{
+    std::ifstream file("input.txt");
+    int aim = 0;
+    unsigned int distance = 0, realDepth = 0;
+    for (std::string dir; file >> dir;) {
+        unsigned int amount;
+        file >> amount;
+        if (!dir.compare("forward")) {
+            distance += amount;
+            realDepth += amount * aim;
+        } else {
+            aim += dir.compare("up") ? amount : -amount;
+        }
+    }
+
+    std::cout << "(Part 1) Direction: " << distance * aim << std::endl;
+    std::cout << "(Part 2) Real Direction: " << distance * realDepth;
+
+    std::cin.get();
+    return 0;
+}
