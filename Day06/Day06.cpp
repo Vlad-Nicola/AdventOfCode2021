5afff455f28331014e70e8539409fc9b0e3fb3e5
Vlad Nicola <vlad.nicola@axigen.com>
2021-12-07 16:00:26 +0200

    Improvements Day06

--- Day06/Day06.cpp
+++ Day06/Day06.cpp
@@ -2,23 +2,19 @@
 #include <fstream>
 #include <vector>
 
-uint64_t offspring(std::vector<uint64_t>& values, const short val, const short days)
+uint64_t offspring(std::vector<uint64_t>& values, const short days)
 {
-    if (val >= days) {
-        return 1;
-    }
-
     uint64_t res = 0;
-    const short spawnedCount = (days - val - 1) / 7 + 1;
+    const short spawnedCount = (days + 6) / 7;
     for (short i = 0; i < spawnedCount; ++i) {
-        const short newDay = days - val - 3 - (7 * i);
+        const short newDay = days - 9 - (7 * i);
         if (newDay <= 0) {
             ++res;
             continue;
         }
         auto& v = values[newDay];
         if (v == 0) {
-            v = offspring(values, 6, newDay);
+            v = offspring(values, newDay);
         }
         res += v;
     }
@@ -28,13 +24,11 @@ uint64_t offspring(std::vector<uint64_t>& values, const short val, const short d
 int main()
 {
     std::ifstream file("input.txt");
-
     std::vector<uint64_t> values(256, 0);
     uint64_t offspringAfter256 = 0, offspringAfter80 = 0;
-    constexpr unsigned short days = 256;
     for (unsigned short newFish; file >> newFish; file.ignore()) {
-        offspringAfter256 += offspring(values, newFish, 256);
-        offspringAfter80 += offspring(values, newFish, 80);
+        offspringAfter256 += offspring(values, 256 - newFish);
+        offspringAfter80 += offspring(values, 80 - newFish);
     }
 
     std::cout << "(Part 1) Fish after 80 days: " << offspringAfter80 << std::endl;
