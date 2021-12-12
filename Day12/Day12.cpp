91dc927a922b69f685909d8732e1db8df00eb0f8
Vlad Nicola <vlad.nicola@axigen.com>
2021-12-12 18:10:56 +0200

    Day12 solution

---
+++ Day12/Day12.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+std::pair<unsigned, unsigned> getPaths(
+    const std::string& elem, 
+    const std::unordered_map<std::string, std::vector<std::string>>& map, 
+    std::vector<std::string> visited,
+    bool allowSmallTwice)
+{
+    if (elem == "end") {
+        return { 1,1 };
+    }
+
+    bool doublePath = false;
+    if (elem[0] > 'Z') {
+        if (std::find(visited.cbegin(), visited.cend(), elem) != visited.cend()) {
+            if (!allowSmallTwice || elem == "start") {
+                return { 0,0 };
+            }
+            allowSmallTwice = false;
+            doublePath = true;
+        } else {
+            visited.push_back(elem);
+        }
+    }
+
+    std::pair<unsigned, unsigned> paths = { 0,0 };
+    for (const auto& neigh : map.at(elem)) {
+        const auto res = getPaths(neigh, map, visited, allowSmallTwice);
+        paths.second += res.second;
+        if (!doublePath) {
+            paths.first += res.first;
+        }
+    }
+    return paths;
+}
+
+int main()
+{
+    std::ifstream file("input.txt");
+    std::unordered_map<std::string, std::vector<std::string>> map;
+    for (std::string line; file >> line;) {
+        const size_t pos = line.find('-');
+        map[line.substr(0, pos)].emplace_back(line.substr(pos + 1));
+        map[line.substr(pos + 1)].emplace_back(line.substr(0, pos));
+    }
+
+    const auto res = getPaths("start", map, {}, true);
+    std::cout << "(Part 1) Visited paths count: " << res.first << std::endl;
+    std::cout << "(Part 2) Visited paths count (one double allowed): " << res.second << std::endl;
+    
+    std::cin.get();
+    return 0;
+}
